0ed5cbcdb22eeac5e941d932ffc9cd27f467e45a
Aakash0023 <itspersonal0623@gmail.com>
2026-04-13 17:58:32 +0530

    Api level data santization

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const connectDB = require("./config/Database");
 const user = require("./models/user");
+
 app.use(express.json());
 app.post("/signup", async (req, res) => {
   const User = new user(req.body);
@@ -11,6 +12,58 @@ app.post("/signup", async (req, res) => {
     res.send("User Added Successfully");
   } catch (err) {
     res.status(400).send("Error saving the user" + err.message);
+  }
+});
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const users = await user.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send("something went wrong");
+  }
+});
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await user.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("something went wrong ");
+  }
+});
+
+app.delete("/delete", async (req, res) => {
+  const userId = req.body.userId;
+  try {
+    const user = await user.findOneAndDelete(userId);
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
+  const data = req.body;
+
+  try {
+    const ALLOWED_UPDATES = ["photourl", "about", "skills", "gender", "age"];
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cant be more than 10");
+    }
+    await user.findByIdAndUpdate({ _id: userId }, data);
+    res.send("user updated sucessfully");
+  } catch {
+    res.status(400).send("Something went wrong");
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,51 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      minLength: 25,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      require: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    gender: {
+      type: String,
+      require: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender is not valid ");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+    },
+    about: {
+      type: String,
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastnName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 const User = mongoose.model("User", userSchema);
 module.exports = User;
